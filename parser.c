e686ac602656f4aeeb6b62cb540e8cde7d119b27
dck9661 <dck9661@icloud.com>
2018-12-03 21:05:22 +0800

    add parser.c

--- parser.c
+++ parser.c
@@ -1,255 +1,134 @@
 #include<stdio.h>
 #include<stdint.h>
 #include"typedefine.h"
+#define target_buffer_len 127
 void parse(logic_signal_packet *in,frame_struct *out)
 {
-	uint32_t stuff_0 = 0x0;
-	uint32_t stuff_1 = 0x1;
-	printf("parser : %X %X %X %X %X %d\n",in->bits[4],in->bits[3],in->bits[2],in->bits[1],in->bits[0],in->packet_len);
-	int count_1 = 0;
-	int count_0 = 0;
-	int N_stuff = 0;
-	uint32_t check=0;	
-	//uint32_t mask = 0xffffffff;
-	uint32_t result[5]={0,0,0,0,0};
-	uint32_t shift = 0;
+	uint32_t count_1 = 0;
+	uint32_t count_0 = 0;
+	uint32_t check = 0;	
+	uint32_t result[4]={0};
 	//remove stuffing bit
-	for(int k=4;k>=0;k--)
-	{
-		for(int i=31;i>=0;i--)
-		{
-			N_stuff++;
-			if(N_stuff <= in->packet_len -10)
-			{
-				count_0 = ((in->bits[k]>>i & stuff_1) == 0) ? count_0++ : 0;
-				count_1 = ((in->bits[k]>>i | stuff_0) == 1) ? count_1++ : 0;
-				check = ((count_0 == 5) || (count_1 == 5)) ? 1 : 0;			
-			}
-			else 
-				check == 0;
-			
-			if(check == 0)
-			{
-				if(count_0 == 5)
-					count_0 = 0;
-				if(count_1 == 5)
-					count_1 = 0;
-		
-				result[k] = result[k] +(in->bits[k] >> i & 0x1);
-				shift++;
-				result[k] = result[k] << shift;				
-			}
-		}
-	}
-	//parser
-	out->SOF = (result[5] >> 31) & 0x1;
-	
+    int i= target_buffer_len;
+    int count = 159;
+    int payload_len = -1;
+    //printf("--------------parse-------------\n");
+    while(i >= 0)
+    {
+        uint8_t cur_bit = (in->bits[count/32] & (0x01 << (count%32))) ? 0x01 : 0x00;
+        //printf("%d ",cur_bit);
+       
+        if( cur_bit != 0)
+        {
+            count_1++;
+            count_0 = 0;
+        }
+        else
+        {
+            count_0++;
+            count_1 = 0;
+        }
+        /*printf("%d: %d( 0x%08X &  0x%08X = 0x%08X), counters = %d, %d\n", 
+                count, cur_bit, in->bits[count/32], (0x01 << (count%32)),
+                (in->bits[count/32] & (0x01 << (count%32))),
+                count_1,count_0);*/
+        if(count_1 == 5 || count_0 == 5)
+        {
+            if(payload_len > 0) {
+                if ( (target_buffer_len - i - 8*payload_len ) < 34 ) 
+                    count --;
+            } else 
+                count--;
+            //printf("\t%d : %d , %d, %d\n", count, cur_bit, count_1, count_0);
+            count_1 = 0;
+            count_0 = 0;
+        }    
+       
+        
+        {
+            result[i/32] |= cur_bit << i%32;
+            /*if(cur_bit)*/
+            //    printf("result[%d] |= 0x%08x (i=%d)\n", i/32, cur_bit<<i%32, i);
+            i--;
+            if(target_buffer_len - i == 19)
+            {
+                payload_len = (result[3] >> 13) & 0xf;
+                if(payload_len > 8)
+                    payload_len = 8;
+                 
+            }
+                
+        }
+        if(target_buffer_len - i == 8*payload_len + 19 + 25)
+            break;
+        count--;
+           
+    }
+    //printf("\n");
+    //printf("result[3]:0x%08X\n",result[3]);
+    //printf("result[2]:0x%08X\n",result[2]);
+    //parser
+    out->SOF = (result[3] >> 31) & 0x1;
+	out->ID_A = 0;
 	for(int i=30;i>=20;i--)
-		out->ID_A = out->ID_A + (((result[5] >>i) & 0x1) << (i-20));
-	out->SRR = (result[5] >> 19) & 0x1;
-	out->IDE = (result[5] >> 18) & 0x1;
-	for(int i=17;i>=0;i--)	
-		out->ID_B = out->ID_B + (((result[5] >>i) & 0x1) << i);
-	out->RTR = (result[4] >> 31) & 0x1;
-	out->Res = ((result[4] >> 30) & 0x1) <<1 + (result[4] >> 29) & 0x1 ;
-	out->DLC = ((result[4] >> 28) & 0x1) <<3 +((result[4] >> 27) & 0x1) <<2 +((result[4] >> 26) & 0x1) <<1 + (result[4] >> 25) & 0x1;
+		out->ID_A = out->ID_A + (((result[3] >>i) & 0x1) << (i-20));
+	out->RTR = (result[3] >> 19) & 0x1;
+	out->IDE = (result[3] >> 18) & 0x1;
+    out->r0 = (result[3] >> 17) & 0x1;
+    out->DLC = (result[3] >> 13) & 0xf;
+	//out->DLC = ((result[3] >> 16) & 0x1) <<3 | ((result[3] >> 15) & 0x1) <<2 | ((result[3] >> 14) & 0x1) <<1 + (result[3] >> 13) & 0x1;
+    //printf("DLC:%d\n",out->DLC);
+    for(int j = 1; j < out->DLC +1; j++ )
+    {
+        uint32_t data_base = target_buffer_len - 19 - 8*j;
+        uint32_t _tmp = 0;
+        if(data_base % 32 > 24 ) {//92
+            _tmp = result[data_base/32] >> (data_base%32 +1);
+            _tmp |= result[data_base/32 +1] << (31 - data_base%32);
+            _tmp &= 0xff;
+        } else {//100
+            _tmp = result[data_base/32] >> (data_base%32 +1);
+            _tmp &= 0xff;
+
+        }
+        //printf("_tmp:0x%02X\n",_tmp);
+    }
 	switch(out->DLC)
 	{
 		case 0:
-		{
 			out->data[0]='\0';
 			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i-10));
-			out->CRC_del = (result[4] >> 9) & 0x1;
-			out->ACK = (result[4] >> 8) & 0x1;
-			out->ACK_del = (result[4] >> 7) & 0x1;
+				out->CRC = out->CRC + (((result[2] >>i) & 0x1) << (i-10));
+			out->CRC_del = (result[2] >> 9) & 0x1;
+			out->ACK = (result[3] >> 8) & 0x1;
+			out->ACK_del = (result[2] >> 7) & 0x1;
 			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[4] >>i) & 0x1) << i);
-		}
+				out->end_of_frame = out->end_of_frame + (((result[2] >>i) & 0x1) << i);
+            break;
 		case 1:
-		{
-			for(int i=24;i>=17;i--)
-				out->data[0] = out->data[0] + (((result[4] >>i) & 0x1) << (i-17));
+		{  
+            out->data[0]='\0';
+			for(int i=12;i>=5;i--)
+            {
+                //printf("(((result[2] >>i) & 0x1):%X:\n",((result[3] >>i) & 0x1));
+				out->data[0] = out->data[0] | (((result[3] >>i) & 0x1) << (i-5));
+            }
 			out->data[1] = '\0';
-			for(int i=16;i>=2;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i-2));
-			out->CRC_del = (result[4] >> 1) & 0x1;
-			out->ACK = (result[4] >> 0) & 0x1;
-			out->ACK_del = (result[3] >> 31) & 0x1;
-			for(int i=30;i>=24;i--)
-				out->end_of_frame = out->end_of_frame + (((result[3] >>i) & 0x1) << (i-24));
+			for(int i=4;i>=0;i--)
+				out->CRC = out->CRC + ((result[3] >>i) & 0x1) << (i+10);
+            for(int i=31;i>=22;i--)
+                out->CRC = out->CRC + ((result[2] >>i) & 0x1) << (i-22);
+			out->CRC_del = (result[2] >> 21) & 0x1;
+			out->ACK = (result[2] >> 20) & 0x1;
+			out->ACK_del = (result[2] >> 19) & 0x1;
+			for(int i=18;i>=12;i--)
+				out->end_of_frame = out->end_of_frame + (((result[2] >>i) & 0x1) << (i-12));
+
+            //printf("data[0]:0x%08X\n",out->data[0]);
+            break;
 		
 		}
-		case 2:
-		{
-			//data
-			for(int i=24;i>=17;i--)
-				out->data[0] = out->data[0] + (((result[4] >>i) & 0x1) << (i-17));
-			for(int i=16;i>=9;i--)
-				out->data[1] = out->data[1] + (((result[4] >>i) & 0x1) << (i-9));
-			//CRC
-			for(int i=8;i>=0;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i+6));
-			for(int i=31;i>=26;i--)
-				out->CRC = out->CRC + (((result[3] >>i) & 0x1) << (i-26));
-			//CRC_del
-			out->CRC_del = (result[3] >> 25) & 0x1;
-			//ACK
-			out->ACK = (result[3] >> 24) & 0x1;
-			//ACK_del
-			out->ACK_del = (result[3] >> 23) & 0x1;
-			//end_of_frame			
-			for(int i=22;i>=16;i--)
-				out->end_of_frame = out->end_of_frame + (((result[3] >>i) & 0x1) << (i-16));
-		}
-		case 3:
-		{
-			//data
-			for(int i=24;i>=17;i--)
-				out->data[0] = out->data[0] + (((result[4] >>i) & 0x1) << (i-17));
-			for(int i=16;i>=9;i--)
-				out->data[1] = out->data[1] + (((result[4] >>i) & 0x1) << (i-9));
-			for(int i=8;i>=1;i--)
-				out->data[2] = out->data[2] + (((result[4] >>i) & 0x1) << (i-1));
-			//CRC
-			out->CRC = out->CRC + ((result[4] >>0) & 0x1) << 14;
-			for(int i=31;i>=18;i--)
-				out->CRC = out->CRC + (((result[3] >>i) & 0x1) << (i-18));
-			//CRC_del
-			out->CRC_del = (result[3] >> 17) & 0x1;
-			//ACK
-			out->ACK = (result[3] >> 16) & 0x1;
-			//ACK_del
-			out->ACK_del = (result[3] >> 15) & 0x1;
-			//end_of_frame		
-			for(int i=14;i>=8;i--)
-				out->end_of_frame = out->end_of_frame + (((result[3] >>i) & 0x1) << (i-8));
-		}
-		case 4:
-		{
-			//data
-			for(int i=24;i>=17;i--)
-				out->data[0] = out->data[0] + (((result[4] >>i) & 0x1) << (i-17)); //data[0] 
-			for(int i=16;i>=9;i--)
-				out->data[1] = out->data[1] + (((result[4] >>i) & 0x1) << (i-9)); //data[1]
-			for(int i=8;i>=1;i--)
-				out->data[2] = out->data[2] + (((result[4] >>i) & 0x1) << (i-1)); //data[2]
-			out->data[3] = out->data[3] + (((result[4] >>0) & 0x1) << 7);		  //data[3][0]	
-			for(int i=31;i>=25;i--)
-				out->data[3] = out->data[3] + (((result[3] >>i) & 0x1) << (i-25)); //data[3][1:7]
-			//CRC
-			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[3] >>i) & 0x1) << (i-10));
-			//CRC_del
-			out->CRC_del = (result[3] >> 9) & 0x1;
-			//ACK
-			out->ACK = (result[3] >> 8) & 0x1;
-			//ACK_del
-			out->ACK_del = (result[3] >> 7) & 0x1;
-			//end_of_frame		
-			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[3] >>i) & 0x1) << i);
-		}
-		case 5:
-		{
-			//data
-			for(int i=24;i>=17;i--)
-				out->data[0] = out->data[0] + (((result[4] >>i) & 0x1) << (i-17)); //data[0] 
-			for(int i=16;i>=9;i--)
-				out->data[1] = out->data[1] + (((result[4] >>i) & 0x1) << (i-9)); //data[1]
-			for(int i=8;i>=1;i--)
-				out->data[2] = out->data[2] + (((result[4] >>i) & 0x1) << (i-1)); //data[2]
-			out->data[3] = out->data[3] + (((result[4] >>0) & 0x1) << 7);		  //data[3][0]	
-			for(int i=31;i>=25;i--)
-				out->data[3] = out->data[3] + (((result[3] >>i) & 0x1) << (i-25)); //data[3][1:7]
-			for(int i=31;i>=25;i--)
-				out->data[4] = out->data[4] + (((result[3] >>i) & 0x1) << (i-25)); //data[3][1:7]
-			//CRC
-			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[3] >>i) & 0x1) << (i-10));
-			//CRC_del
-			out->CRC_del = (result[3] >> 9) & 0x1;
-			//ACK
-			out->ACK = (result[3] >> 8) & 0x1;
-			//ACK_del
-			out->ACK_del = (result[3] >> 7) & 0x1;
-			//end_of_frame		
-			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[3] >>i) & 0x1) << i);
-		}
-		case 6:
-		{
-			out->data[0]='\0';
-			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i-10));
-			out->CRC_del = (result[4] >> 9) & 0x1;
-			out->ACK = (result[4] >> 8) & 0x1;
-			out->ACK_del = (result[4] >> 7) & 0x1;
-			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[4] >>i) & 0x1) << i);
-		}
-		case 7:
-		{
-			out->data[0]='\0';
-			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i-10));
-			out->CRC_del = (result[4] >> 9) & 0x1;
-			out->ACK = (result[4] >> 8) & 0x1;
-			out->ACK_del = (result[4] >> 7) & 0x1;
-			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[4] >>i) & 0x1) << i);
-		}
-		default:
-		{
-			out->data[0]='\0';
-			for(int i=24;i>=10;i--)
-				out->CRC = out->CRC + (((result[4] >>i) & 0x1) << (i-10));
-			out->CRC_del = (result[4] >> 9) & 0x1;
-			out->ACK = (result[4] >> 8) & 0x1;
-			out->ACK_del = (result[4] >> 7) & 0x1;
-			for(int i=6;i>=0;i--)
-				out->end_of_frame = out->end_of_frame + (((result[4] >>i) & 0x1) << i);
-		}
-	}
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-	/*for(int i=31;i>=0;i--)
-	{
-		
-		
-		count_0 = ((a>>i & stuff_1) == 0) ? count_0++ : 0;
-		count_1 = ((a>>i | stuff_0) == 1) ? count_1++ : 0;				
-		mask = mask >> 1;
-		 
-		if(count_0 == 5)
-		{	
-			shift++;
-			mask1 = mask >> 1 ;
-			b = b & ~mask1;
-			b = ((a & mask1) << shift) + (a & ~mask); 
-			count_0 = 0;
-			
-		}
-		else if(count_1 == 5)
-		{
-			shift++;
-			mask1 = mask >> 1 ;
-			b = ((a & mask1) << shift) + (a & ~mask);
-			count_1 = 0;
-		}
-			
-			
-	}*/
-	
 
