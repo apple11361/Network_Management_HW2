83f550cb6f329700cde4040bdd364470e1af1ddc
XzSmile <apple113611361@gmail.com>
2018-12-04 11:42:02 +0800

    Bugfix: fix the multiple definition
    You should not include *.c files.

--- error_checking.c
+++ error_checking.c
@@ -5,107 +5,95 @@
 
 int bit_stuffing_check(logic_signal_packet* input)
 {
-	uint32_t len = input->packet_len;
-	uint32_t find_byte = len / 32;
-	uint32_t find_bit = 32;
-	uint32_t count = 0;
+    uint32_t len = input->packet_len;
+    uint32_t find_byte = len / 32;
+    uint32_t find_bit = 32;
+    uint32_t count = 0;
 
-	for(int i = 0; i <= find_byte; i++)
-	{
-		if(i == find_byte) find_bit = len % 32;
-		for(int j = 0; j < find_bit; j++)
-		{
-			if((input->bits[i] >> j) & 1 == 1) count = 0;
-			else count++;
+    for(int i = 0; i <= find_byte; i++) {
+        if(i == find_byte) find_bit = len % 32;
+        for(int j = 0; j < find_bit; j++) {
+            if((input->bits[i] >> j) & 1 == 1) count = 0;
+            else count++;
 
-			if(count > 5) return 0;
-		}
-	}
-	return 1;
- }
+            if(count > 5) return 0;
+        }
+    }
+    return 1;
+}
 
 int frame_check(logic_signal_packet* input)
 {
-	uint32_t len = input->packet_len;
-	uint32_t find_byte = len / 32;
-	uint32_t find_bit = 32;
-	uint32_t count = 0;
-	uint32_t fill = 0;
-	uint32_t position = 0;
- 	uint32_t check_bound = len - 10;
-	bool fill_flag = 0;
+    uint32_t len = input->packet_len;
+    uint32_t find_byte = len / 32;
+    uint32_t find_bit = 32;
+    uint32_t count = 0;
+    uint32_t fill = 0;
+    uint32_t position = 0;
+    uint32_t check_bound = len - 10;
+    bool fill_flag = 0;
 
-	for(int i = 0; i <= find_byte; i++)
-	{
-		if(i == find_byte) find_bit = len % 32;
-		for(int j = 0; j < find_bit; j++)
-		{
-			if((input->bits[i] >> j) & 1 == 1) count = 0;
-			else count++;
+    for(int i = 0; i <= find_byte; i++) {
+        if(i == find_byte) find_bit = len % 32;
+        for(int j = 0; j < find_bit; j++) {
+            if((input->bits[i] >> j) & 1 == 1) count = 0;
+            else count++;
 
-			position = i * 32 + j - fill;
+            position = i * 32 + j - fill;
 
-			if(fill_flag == 1)
-			{
-				fill_flag = 0;
-				continue;
-			}
+            if(fill_flag == 1) {
+                fill_flag = 0;
+                continue;
+            }
 
-			if(position == 12 || position == 13 || position == 14)
-			{
-				if((input->bits[i] >> j) & 1 == 1) return -1;
-			}
-			if((i * 32 + j) >= check_bound)
-			{
-				if((input->bits[i] >> j) & 1 == 0) return -1;
-			}
+            if(position == 12 || position == 13 || position == 14) {
+                if((input->bits[i] >> j) & 1 == 1) return -1;
+            }
+            if((i * 32 + j) >= check_bound) {
+                if((input->bits[i] >> j) & 1 == 0) return -1;
+            }
 
-			if(count == 5)
-			{
-				fill++;
-				fill_flag = 1;
-			}
-		}
-	}
-	return 0;
+            if(count == 5) {
+                fill++;
+                fill_flag = 1;
+            }
+        }
+    }
+    return 0;
 }
 
 uint16_t can_crc_next(uint16_t crc, uint8_t data)
 {
-  uint8_t i, j;
+    uint8_t i, j;
 
-  crc ^= (uint16_t)data << 7;
+    crc ^= (uint16_t)data << 7;
 
-  for (i = 0; i < 8; i++)
-  {
-    crc <<= 1;
-    if (crc & 0x8000)
-    {
-      crc ^= 0xc599;
+    for (i = 0; i < 8; i++) {
+        crc <<= 1;
+        if (crc & 0x8000) {
+            crc ^= 0xc599;
+        }
     }
-  }
 
-  return crc & 0x7fff;
+    return crc & 0x7fff;
 }
 
 int crc_check(frame_struct* input)
 {
-  int data_num = input->DLC;
-	uint8_t crc_input[11];
-	for(int i = 0; i < data_num; i++)
-	{
-		crc_input[i + 3] = input->data[data_num - 1 - i];
-	}
-	crc_input[2] = (uint8_t)((input->ID_A & 1) << 7) + data_num;
-	crc_input[1] = (uint8_t)(input->ID_A >> 1);
-	crc_input[0] = (uint8_t)(input->ID_A >> 9);
+    int data_num = input->DLC;
+    uint8_t crc_input[11];
+    for(int i = 0; i < data_num; i++) {
+        crc_input[i + 3] = input->data[data_num - 1 - i];
+    }
+    crc_input[2] = (uint8_t)((input->ID_A & 1) << 7) + data_num;
+    crc_input[1] = (uint8_t)(input->ID_A >> 1);
+    crc_input[0] = (uint8_t)(input->ID_A >> 9);
 
-  uint16_t crc;
-  crc = 0;
+    uint16_t crc;
+    crc = 0;
 
-  for (int i = 0; i < data_num + 3; i++)
-	{
-    crc = can_crc_next(crc, crc_input[i]);
-  }
-	return crc;
+    for (int i = 0; i < data_num + 3; i++) {
+        crc = can_crc_next(crc, crc_input[i]);
+    }
+    return crc;
 }
--- signal_converter.c
+++ signal_converter.c
@@ -2,9 +2,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "typedefine.h"
-#include "parser.c"
-#include "error_checking.c"
-
 
 int receive();
 int signal_convert(int h, int l);
@@ -14,7 +11,7 @@ int receive()
 {
     int i;
     int count=0;
-	FILE *H = fopen("test_case/High.txt","r");
+    FILE *H = fopen("test_case/High.txt","r");
     FILE *L = fopen("test_case/Low.txt","r");
     char chH,chL;
     int result;
@@ -22,37 +19,33 @@ int receive()
     logic_signal_packet *packet = malloc(sizeof(logic_signal_packet));
     frame_struct *result_packet = malloc(sizeof(frame_struct));
     packet->packet_len=0;
-    for(i=0;i<5;i++)
+    for(i=0; i<5; i++)
         packet->bits[i]=0;
-    if( NULL == H ){ 
+    if( NULL == H ) {
         printf( "High.txt open failure\n" );
         return 0;
-    }
-    else if( NULL == L ) {
+    } else if( NULL == L ) {
         printf( "Low.txt open failure\n" );
         return 0;
-    }
-    else {
+    } else {
         i=159;
         int temp1;
         int temp2;
-        while((chH = fgetc(H)) != EOF) { 
+        while((chH = fgetc(H)) != EOF) {
             chL = fgetc(L);
-            if(chH != ' '){
+            if(chH != ' ') {
                 if(chH=='1')temp1=1;
                 else temp1=0;
-                if(chL=='-'){
+                if(chL=='-') {
                     temp2=-1;
                     fgetc(L);
-                }
-                else temp2=0;
-                if(chH != '\n'){
+                } else temp2=0;
+                if(chH != '\n') {
                     result = signal_convert(temp1,temp2);
                     packet->bits[i/32] = packet->bits[i/32] + (result << (i%32));
                     temp = i;
                     i--;
-                }
-                else {
+                } else {
                     packet->packet_len = 160 - temp;
                     parse(packet,result_packet);
                     if(bit_stuffing_check(packet)==-1)
@@ -61,9 +54,9 @@ int receive()
                         printf("frame_check Error\n");
                     else if(crc_check(result_packet)==-1)
                         printf("crc_check Error\n");
-                    else 
+                    else
                         save_data(result_packet,++count, 160 - temp);
-                    for(i=0;i<5;i++)
+                    for(i=0; i<5; i++)
                         packet->bits[i]=0;
                     packet->packet_len=0;
                     i=159;
@@ -78,20 +71,21 @@ int receive()
             printf("frame_check Error\n");
         else if(crc_check(result_packet)==-1)
             printf("crc_check Error\n");
-        else 
+        else
             save_data(result_packet,++count, 160 - temp);
     }
 }
 
 int signal_convert(int h, int l)
 {
-	short result = 0;
-	result=	(h==1 && l==-1) ? 0 : 
-			(h==0 && l== 0) ? 1 : -1;
-	return result;
+    short result = 0;
+    result=	(h==1 && l==-1) ? 0 :
+            (h==0 && l== 0) ? 1 : -1;
+    return result;
 }
 
 void save_data(frame_struct* frame , int count, int packet_length)
 {
-	printf("ID:%2d, data:%s, packet count:%d, packet length:%d\n", frame->ID_A, frame->data, count, packet_length);
+    printf("ID:%2d, data:%s, packet count:%d, packet length:%d\n", frame->ID_A,
+           frame->data, count, packet_length);
 }
